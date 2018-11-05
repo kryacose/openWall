b52f9c64f002af969bc01dc9b1c257eb485df7d6
kryacose <kuriakose.el@gmail.com>
2018-11-05 17:39:43 +0800

    commit2

--- DB.py
+++ DB.py
@@ -2,8 +2,29 @@ import sqlite3
 
 class DB:
 
-    def __init__(dbloc):
-        self.dbloc = dbloc
+    def __init__(self, db_loc):
+        self.db_loc = db_loc
 
-    def fullRefresh():
+
+    def execute(self, query):
+        conn = sqlite3.connect(self.db_loc)
+        c = conn.cursor()
+        try:
+            r=c.execute(query)
+        except Exception as e:
+            return e
+        else:
+            res = r.fetchall()
+            conn.commit()
+            conn.close()
+            return res
+        
+
+
+    def fullRefresh(self):
+        self.execute("drop table if exists users")
+        self.execute("drop table if exists posts")
+
+        self.execute("create table users( username varchar(25) primary, password varchar(25), dob date)")
+        self.execute("create table posts( postid int, username varchar(25), )")
         
