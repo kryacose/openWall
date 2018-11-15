5686162451110d74879ca4491dffa4936f072f99
kryacose <kuriakose.el@gmail.com>
2018-11-15 20:42:59 +0800

    Database backend updated

--- DB.py
+++ DB.py
@@ -12,6 +12,7 @@ class DB:
         try:
             r=c.execute(query)
         except Exception as e:
+            conn.close()
             return e
         else:
             res = r.fetchall()
@@ -21,10 +22,78 @@ class DB:
         
 
 
-    def fullRefresh(self):
+    def Refresh(self):
         self.execute("drop table if exists users")
         self.execute("drop table if exists posts")
 
-        self.execute("create table users( username varchar(25) primary, password varchar(25), dob date)")
-        self.execute("create table posts( postid int, username varchar(25), )")
+        self.execute('''create table users(
+                        username varchar(50) PRIMARY KEY,
+                        pw varchar(50),
+                        fname varchar(100),
+                        lname varchar(100),
+                        dob date,
+                        sex varchar(20),
+                        aboutme varchar(200)
+                    );''')
+
+
+        self.execute('''create table posts(
+                        postid int PRIMARY KEY,
+                        username varchar(50),
+                        timestmp timestamp,
+                        likes int,
+                        postdescpt varchar(200)
+                    );''')
         
+
+    #add post
+    def addPost(self, username, desc):
+        postid = self.execute('select count(*) from posts;')[0][0]
+        q = f"insert into posts values( {postid}, '{username}', datetime('now','localtime'), 0 ,'{desc}');"
+
+        print(q)
+        return self.execute(q)
+
+    def likePost(self, postid):
+        q=  f"UPDATE posts SET likes = likes + 1 WHERE postid = {postid};"
+        print(q)
+        return self.execute(q)
+
+
+    #add users
+    def addUser(self, username, password, fname, lname, date, sex, desc):
+        q = f"insert into users values('{username}', '{password}', '{fname}', '{lname}', '{date}', '{sex}', '{desc}');"
+        return self.execute(q)
+
+
+    #list users
+    def listUser(self):
+        q = "select username, fname, lname, dob, sex, aboutme from users;"
+        return self.execute(q)
+
+
+    #get User data
+    def getUserInfo(self, username):
+        q = f"select username, fname, lname, dob, sex, aboutme from users where username = '{username}';"
+        return self.execute(q)
+
+
+    #list posts
+    def listAllPost(self):
+        q = "select * from posts order by timestmp;"
+        return self.execute(q)
+
+    #list user posts
+    def listUserPost(self, username):
+        q = f"select * from posts where username ='{username}' order by timestmp;"
+        print(q)
+        return self.execute(q)
+
+    #verify user
+    def login(self, username, password):
+        q = f"select pw from users where username = '{username}';"
+        r = self.execute(q)
+        # print(r,[(password,)])
+        if(r == [(password,)]):
+            return 1
+        return 0
